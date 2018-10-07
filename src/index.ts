9fda5dae8b113987b67a0a9d514961d0d26f053e
tibawatanabe <tibawatanabe@gmail.com>
2018-10-07 20:55:19 -0300

    Setup dialog-flow

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
+import { json } from 'body-parser';
+import { config } from 'dotenv';
 import * as express from 'express';
 
-import { config } from 'dotenv';
+import { app } from './app';
 
 // loading '.env'
 config();
 
 const { PORT } = process.env;
 
-const app = express();
+const server = express();
 
-app.get('/', (_req, res) => { res.send('Hello world'); });
+server.use(json());
 
-app.listen(PORT, () => console.info(`Server listening on: ${PORT}`));
+server.get('/', (_req, res) => res.send('Hello World!'));
+
+server.post('/fulfillment', app());
+
+server.listen(PORT, () => console.info(`Server listening on: ${PORT}`));
