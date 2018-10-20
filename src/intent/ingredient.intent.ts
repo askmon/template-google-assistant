b551aef9a2814f5200aac1cbbcdfc42a332f90ce
Arthur Fonseca <flavio.mello1-trc@multiplus.corp>
2018-10-20 16:18:56 -0300

    altercao ingrediente e receita

--- src/intent/ingredient.intent.ts
+++ src/intent/ingredient.intent.ts
@@ -3,12 +3,19 @@ import { ssml } from '~/ssml';
 export const ingredientIntent: DialogFlowIntent = {
   name: 'ingrediente',
   handler: (conv) => {
-    conv.data.ingredientIndex = 0;
-    conv.data.ingredients = ["Uma xícara de chá de arroz", "Duas xícaras de chá de água", "Uma xícara de chá de leite", "Uma colher de sopa de manteiga", "Uma colher de chá de sal"]
+    // tslint:disable-next-line:no-string-literal
+    conv.data['ingredientIndex'] = 0;
+    // tslint:disable-next-line:no-string-literal
+    conv.data['ingredients'] = ['Uma xícara de chá de arroz',
+                                'Duas xícaras de chá de água',
+                                'Uma xícara de chá de leite',
+                                'Uma colher de sopa de manteiga',
+                                'Uma colher de chá de sal'];
     conv.ask(
       ssml`
-      <speak>"${conv.data.ingredients[conv.data.ingredientIndex]}"<break time="500ms"/></speak>
+      <speak>"${conv.data['ingredients'][conv.data['ingredientIndex']]}"<break time="500ms"/></speak>
       `,
     );
+    conv.data['ingredientIndex'] = conv.data['ingredientIndex'] + 1;
   },
 };
